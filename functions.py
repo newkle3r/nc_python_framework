3842ef95f09f4dc7cc24e1c06511b940d6a43dd3
newkle3r <117505384+newkle3r@users.noreply.github.com>
2024-06-03 01:18:10 +0200

    Update functions.py

--- functions.py
+++ functions.py
@@ -15,18 +15,18 @@ from typing import Any, Callable, Dict, List, Type, TypeVar, cast
 # --------------------------------
 
 nextcloud_config_path = "/var/www/nextcloud/config/config.php"
-ledger_config_path = "/var/scripts/service-API/nc_python_framework/env_variables.json"
+ledger_config_path = "/var/scripts/service-API/env_variables.json"
 with open(ledger_config_path, 'r') as file:
     env_vars = json.load(file)
 
 
 def save_env_variables(env_vars):
-    with open('/var/scripts/service-API/nc_python_framework/env_variables.json', 'w') as file:
+    with open('/var/scripts/service-API/env_variables.json', 'w') as file:
         json.dump(env_vars, file, indent=4)
 
 def get_env_variables():
-    if os.path.exists('env_variables.json'):
-        with open('/var/scripts/service-API/nc_python_framework/env_variables.json', 'r') as file:
+    if os.path.exists('/var/scripts/service-API/env_variables.json'):
+        with open('/var/scripts/service-API/env_variables.json', 'r') as file:
             return json.load(file)
     return {}
 
@@ -66,12 +66,91 @@ def check_file_permissions(file_path):
     print(f"Is writable: {bool(st.st_mode & stat.S_IWUSR)}")
     print(f"Is executable: {bool(st.st_mode & stat.S_IXUSR)}")
 
-def gco(command: str) -> str:
-    return subprocess.check_output(command, shell=True).decode('utf-8').strip()
+#def gco(command: str) -> str:
+#    return subprocess.check_output(command, shell=True).decode('utf-8').strip()
 
+def gco(command):
+    try:
+        result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        if result.returncode != 0:
+            raise RuntimeError(f"Command failed: {result.stderr}")
+        return result.stdout.strip()
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+
+def check_docker_daemon():
+    try:
+        subprocess.run("docker info", shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        return True
+    except subprocess.CalledProcessError:
+        print("Docker daemon is not running. Attempting to start Docker...")
+
+        # Start Docker daemon
+        try:
+            gco("sudo systemctl start docker")
+            print("Docker daemon started.")
+        except RuntimeError:
+            print("Failed to start Docker daemon.")
+            return False
+
+        # Enable Docker daemon at boot
+        try:
+            gco("sudo systemctl enable docker")
+            print("Docker daemon enabled to start at boot.")
+        except RuntimeError:
+            print("Failed to enable Docker daemon at boot.")
+            return False
+
+        # Check if user is in the docker group
+        user = os.getenv("USER")
+        groups = gco(f"groups {user}")
+        if "docker" not in groups:
+            try:
+                gco(f"sudo usermod -aG docker {user}")
+                print(f"User {user} added to docker group. Please log out and log back in for the changes to take effect.")
+                return False  # Ask user to log out and log back in
+            except RuntimeError:
+                print(f"Failed to add user {user} to docker group.")
+                return False
+
+        # Set permissions for Docker socket
+        try:
+            gco("sudo chown root:docker /var/run/docker.sock")
+            gco("sudo chmod 660 /var/run/docker.sock")
+            print("Docker socket permissions set.")
+        except RuntimeError:
+            print("Failed to set Docker socket permissions.")
+            return False
+
+        # Restart Docker daemon
+        try:
+            gco("sudo systemctl restart docker")
+            print("Docker daemon restarted.")
+        except RuntimeError:
+            print("Failed to restart Docker daemon.")
+            return False
+
+        # Check and set DOCKER_HOST environment variable
+        docker_host = os.getenv("DOCKER_HOST")
+        if docker_host != "unix:///var/run/docker.sock":
+            os.environ["DOCKER_HOST"] = "unix:///var/run/docker.sock"
+            print("DOCKER_HOST environment variable set to unix:///var/run/docker.sock")
+
+        # Verify Docker daemon status
+        try:
+            gco("sudo systemctl status docker")
+            print("Docker daemon is running.")
+            return True
+        except RuntimeError:
+            print("Docker daemon is not running.")
+            return False
+        
 def generate_strong_password(length: int = 16) -> str:
-    characters = string.ascii_letters + string.digits + string.punctuation
-    return ''.join(secrets.choice(characters) for i in range(length))
+    # Exclude single quote (') and double quote (") for the docker-compose template so that the password can be used as an environment variable
+    characters = string.ascii_letters + string.digits + "!#$%&()*+,-./:;<=>?@[\\]^_`{|}~"
+    password = ''.join(secrets.choice(characters) for i in range(length))
+    return password.replace('$', '$$')  # Escape the $ character for docker-compose
 
 def get_default_time_zone():
     return gco("timedatectl show -p Timezone --value")
